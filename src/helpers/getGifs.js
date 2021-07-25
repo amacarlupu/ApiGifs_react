cbc3316396b9312e7a14afbfc41d3f81eff62f3f
Anderson Joel <amacarlupup@gmail.com>
2021-07-25 03:52:57 -0500

    Archivo html actualizado

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export const getGifs = async ( category ) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=7604fzMdoAN0btX6yoatfgHuP6XhQJG2`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=${ yourApiKey }`;
     const resp = await fetch(url);
     const { data } = await resp.json(); // solo obtiene el arreglo data
 
